7fc0b47e17333be81974e0495f911ee90763c912
Paige Gorry <paigeegorry@gmail.com>
2019-01-18 14:36:38 -0800

    able to get my tweets

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./lib/app');
 
 app.listen(7890, () => {
   console.log('listening on port 7890');
---
+++ test/app.test.js
@@ -0,0 +1,53 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+
+const createTweet = (handle) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text: 'oink tweet moo'
+    });
+};
+
+describe('tweets', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ handle: 'paige', text: 'TWEET' })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'paige',
+          text: 'TWEET',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('gets all the tweets', () => {
+    const tweetsToCreate = ['yoyo', 'jelly123', 'jessie456'];
+    return Promise.all(tweetsToCreate.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets')
+          .then(({ body }) => {
+            expect(body).toHaveLength(3);
+          });
+      });
+  });
+
+});
