f0af6af55c3e3f30684d5fd388555fc9c6d6845a
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-18 14:39:19 -0800

    get all tweets works, test passes

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,35 @@
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const createTweet = (handle, text) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text
+    })
+    .then(res => res.body);
+};
+
+// APP ------------------------------------------
 describe('tweets', () => {
+  beforeAll(done => {
+    createTweet('hi I a tweet');
+    mkdirp('./data/tweets', done);
+    done();
+  });
+  afterEach(done => {
+    rimraf('./data/tweets/*', done);
+    done();
+  });
+  afterAll(done => {
+    createTweet('hi I a tweet');
+    done();
+  });
+
+  // CREATE ------------------------------------------
   it('gets a tweet', () => {
     return request(app)
       .post('/tweets')
@@ -14,4 +42,17 @@ describe('tweets', () => {
         });
       });
   });
+  // GET LIST ------------------------------------------
+  it('gets a list of tweets from db', () => {
+    const tweetsToCreate = ['hey', 'hi', 'hello', 'hola'];
+    return Promise.all(tweetsToCreate.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
+      });
+  });
+
 });
