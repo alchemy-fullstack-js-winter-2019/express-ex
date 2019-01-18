55f718861eb40f250eff5cf99fb072a5351f93fd
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-18 14:31:09 -0800

    failing test written

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./lib/app');
 
 
 app.listen(7890, () => {
+  // eslint-disable-next-line no-console
   console.log('running');
 });
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,16 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const createTweet = (name) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      name: name,
+      text: 'my first tweet'
+    })
+    .then(res => res.body);
+};
+
 describe('tweets', () => {
   it('posts a tweet', () => {
     return request(app)
@@ -12,6 +22,17 @@ describe('tweets', () => {
           text: 'my first tweet', 
           _id: expect.any(String)
         });
+      });
+  });
+  it('gets a list of tweets', () => {
+    const tweetsToMake = ['tyler', 'ryan', 'frank'];
+    return Promise.all(tweetsToMake.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(3);
       });
   });
 });
