aa35c015dc51c2ff12fb2219aa36e46d91694364
pickuppatterns <pickup.patterns@gmail.com>
2019-01-18 14:20:12 -0800

    adds post test

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,5 +4,16 @@ const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
 describe('express server', () => {
-  it('gets one of something', () =>  )
-})
+  it('creates a new tweet', () =>  
+  return request(app)
+  .post('/tweet')
+  .send({ handle: 'mike', text:'my 1st tweet' }
+  .then(res => {
+    expect(res.body).toEqual({
+      handle:'mike',
+      text:'my 1st tweet',
+      _id: expect.any(String)
+    });
+  }))
+});
+});
