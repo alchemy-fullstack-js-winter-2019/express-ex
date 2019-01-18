3949ec87aa86f0def27b8e9d2ee5af98359c7342
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-18 14:32:24 -0800

    list of tweets and post methods/test working

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
 describe('tweets', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
   it('gets a tweet', () => {
     return request(app)
       .get('/tweets/abcd')
@@ -21,6 +35,26 @@ describe('tweets', () => {
         expect(res.body).toEqual({
           handle: 'Aaron',
           text: 'Hey there',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('gets a list of all tweets', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'AMDennis87',
+        text: 'Nooo'
+      })
+      .send({
+        handle: 'DMAaron87',
+        text: 'Yesss'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'DMAaron87',
+          text: 'Yesss',
           _id: expect.any(String)
         });
       });
