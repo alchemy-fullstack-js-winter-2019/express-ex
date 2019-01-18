e6140e2de14a870ca0a9b4b46ebd0382482ac715
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-18 15:22:52 -0800

    passing all tests except update

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,96 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+
+const createTweet = (handle) => {
+  return request(app)
+    .post('/tweets')
+    .send({ 
+      handle: handle,
+      tweet: 'test tweet'
+    })
+    .then(res => res.body);
+};
 
 describe('tweets', () => {
-  it('gets a tweet', () => {
+  beforeEach((done) => {
+    rimraf('./lib/data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach((done) => {
+    mkdirp('./lib/data/tweets', err => {
+      done(err);
+    });
+  });
+
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'ghostrider',
+        tweet: 'longboarding life yo'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ghostrider',
+          tweet: 'longboarding life yo',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('can list all the tweets in the database', () => {
+    const handles = ['roxy1', 'roxy2', 'roxy3', 'roxy4'];
+    return Promise.all(handles.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
+      });
+  });
+  it('gets a tweet by id', () => {
     return request(app)
       .get('/tweets/abcd')
       .then(res => {
         expect(res.text).toEqual('abcd');
       });
   });
+  it('updates a tweet with :id and returns the update', () => {
+    return createTweet('kristin1')
+      .then((createdTweet) => {
+        const id = createdTweet._id;
+        const updatedTweet = {
+          handle: 'roxius',
+          tweet: 'some tweet'
+        };
+        return request(app)
+          .put(`/tweets/${id}`)
+          .send(updatedTweet)
+          .then(res => {
+            console.log(updatedTweet);
+            console.log(res.body);
+
+            expect(res.body).toEqual({
+              handle: 'roxius',
+              tweet: 'some tweet'
+            });
+          });
+      });
+  });
+  it('deletes a tweet with :id and returns the delete count', () => {
+    return createTweet('baller for lyfe')
+      .then((createdTweet) => {
+        const id = createdTweet._id;
+        return request(app)
+          .delete(`/tweets/${id}`)
+          .then(res => {
+            expect(res.body).toEqual({ 'deleted': 1 });
+          });
+      });
+  });
 });
+
