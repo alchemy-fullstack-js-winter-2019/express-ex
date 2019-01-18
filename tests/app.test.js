204a693ef29d51fb7d39efaec47bb6030ec41c5e
pickuppatterns <pickup.patterns@gmail.com>
2019-01-18 14:14:53 -0800

    added testing dev dependencies

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
 describe('express server', () => {
   it('gets one of something', () =>  )
