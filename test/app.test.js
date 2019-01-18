7302af81982e62795b5556cd6fd050e7ec5eb864
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-18 15:45:32 -0800

    five methods getbyid/post/updatebyid/deletebyid/getall

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,18 @@ const request = require('supertest');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
+const createTweet = (handle) => {
+    return request(app)
+        .post('/tweets')
+        .send({
+            tweet: 'KAAACAHHH',
+            handle: handle
+        })
+        .then(res => {
+            return res.body;
+        });
+};
+
 describe('test tweets', () => {
     beforeEach(done => {
         rimraf('./data/tweets', err => {
@@ -24,6 +36,42 @@ describe('test tweets', () => {
             })
             .then(res => {
                 expect(res.body.handle).toContain('lance');
+            });
+    });
+
+    it('can gets all', () => {
+        const handlesToCreate = ['kananiboy', 'kananiboy2', 'kananiboy3'];
+        return Promise.all(handlesToCreate.map(createTweet))
+            .then(() => {
+                return request(app)
+                    .get('/tweets');
+            })
+            .then(({ body }) => {
+                expect(body).toHaveLength(3);
+            });
+    });
+
+    it('it can update a tweet', () => {
+        return createTweet('LANCE')
+            .then(createdTweet => {
+                createdTweet.handle = 'lanceUPDATED';
+                return request(app)
+                    .put(`/tweets/${createdTweet._id}`)
+                    .send(createdTweet);
+            })
+            .then(res => {
+                expect(res.text).toContain('lanceUPDATED');
+            });
+    });
+
+    it('can delete a tweet by id', () => {
+        return createTweet('lance63')
+            .then(createdTweet => {
+                return request(app)
+                    .del(`/tweets/${createdTweet._id}`);
+            })
+            .then(res => {
+                expect(res.body).toEqual({ "deleted": 1 });
             });
     });
 
