086d5094695a25dd385a093425654eeda738dbff
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-18 14:21:13 -0800

    post method works

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,39 @@
 const app = require('../lib/app');
-const request = require('suoertest');
+const request = require('supertest');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
 describe('test tweets', () => {
-    it('can get tweets', () => {
+    beforeEach(done => {
+        rimraf('./data/tweets', err => {
+            done(err);
+        });
+    });
+    beforeEach(done => {
+        mkdirp('./data/tweets', err => {
+            done(err);
+        });
+    });
+    
+    it('can post a tweet', () => {
+        return request(app)
+            .post('/tweets')
+            .send({
+                tweet: 'KAAACAHHH',
+                handle: 'lance'
+            })
+            .then(res => {
+                expect(res.body.handle).toContain('lance');
+            });
+    });
+
+
+    it('can get a tweet', () => {
         return request(app)
             .get('/tweets/abcd')
             .then(res => {
-                expect(res.body).toEqual('abcd');
+                expect(res.text).toEqual('abcd');
             });
     });
+
 });
