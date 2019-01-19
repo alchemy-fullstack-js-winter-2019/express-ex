b3d978a226db815872a25cafa2b63483035842f0
avanvalk <drewvv@gmail.com>
2019-01-18 17:02:53 -0800

    added GET methods

--- test/models/app.test.js
+++ test/models/app.test.js
@@ -1,17 +1,53 @@
-const request = request('supertest');
+const request = require('supertest');
 const app = require('../../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
+const createTweet = handle => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text: 'my first tweet',
+      _id: expect.any(String)
+    })
+    .then(res => res.body);
+};
 
 describe('tweets', () => {
-  it('gets a tweet', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  it('creates a tweet', () => {
     return request(app)
-      .get('/tweets')
+      .post('/tweets')
       .send({ handle: 'ryan', text: 'my first tweet' })
       .then(res => {
-        expect(res.text).toEqual({
+        expect(res.body).toEqual({
           handle: 'ryan',
           text: 'my first tweet',
           _id: expect.any(String)
         });
+      });
+  });
+  it('gets a list of tweets from our db', () => {
+    const tweetsToCreate = ['tweet A', 'tweet B', 'tweet C', 'tweet D'];
+    return Promise.all(tweetsToCreate.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
       });
   });
 });
