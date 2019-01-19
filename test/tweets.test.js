68bbbba14c32d66564ddbfe4b3c2d39b1c12cbd1
Shaba9 <shaba92@gmail.com>
2019-01-19 12:16:50 -0800

    beforeEaches added to test

--- test/tweets.test.js
+++ test/tweets.test.js
@@ -1,7 +1,33 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+let tweet = null;
 describe('tweets', () => {
+  beforeEach(done => {
+    if(!tweet) {
+      done();
+    } 
+    else {
+      request(app)
+        .del(`/tweets/${tweet._id}`)
+        .then(() => done());
+    }
+  });
+
+  beforeEach(done => {
+    request(app)
+      .post('/tweets')
+      .send({
+        handle: 'shabz',
+        text: 'I am a twit',
+        _id:'1'
+      })
+      .then(res => {
+        tweet = res.body;
+        done();
+      });
+  });
+
   it('posts a tweet', () => {
     return request(app)
       .post('/tweets')
@@ -16,4 +42,6 @@ describe('tweets', () => {
         _id:'1'
       }));
   });
+
+
 });
