e24be24af023f5a1f1c6fdf98cb65dd222d2af3b
Carmen V Ramos <carmenvramos@yahoo.com>
2019-01-18 17:14:06 -0800

    Added Tests (create tweet and list tweets) but still failing

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,66 @@
-const request = request('supertest');
+const request = require('supertest');
 const app = require('../lib/app');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
+const createTweet = handle => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text: 'hi there'
+    })
+    .then(res => res.body);
+};
 
 describe('tweets', () => {
-  it('gets a tweet', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  // it('gets a tweet', () => {
+  //   return request(app)
+  //     .get('/tweets/abcd')
+  //     .then(res => {
+  //       expect(res.text).toEqual('abcd');
+  //     });
+  // });
+
+  it('creates a new tweet', () => {
     return request(app)
-      .get('/tweets/abcd')
+      .post('/tweets')
+      .send({ handle: 'carmen', text: 'my first tweet' })
       .then(res => {
-        expect(res.text).toEqual('abcd');
+        expect(res.body).toEqual({
+          handle: 'carmen', text: 'my first tweet', _id: expect.any(String)
+        });
+      });
+  });
+
+  it('gets a list of tweets', () => {
+    const tweetsToCreate = ['carmen1', 'carmen2', 'carmen3'];
+
+    return Promise.all(tweetsToCreate.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(3);
       });
   });
 });
+
+//   it('gets a tweet by id', () => {
+//     return request(app)
+//       .get('/tweets')
+//   });
+// });
