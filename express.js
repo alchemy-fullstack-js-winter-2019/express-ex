a32c68521094571f6d1c69a24d130710894e4105
Dyanna Tolman <dyannast@gmail.com>
2019-01-19 09:20:45 -0800

    created files post test and method works get by id and get tweets works

---
+++ express.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+const express = require('express');
+const app = express();
+
+app.listen(7890, () => {
+  console.log('running');
+});
---
+++ test/app.test.js
@@ -0,0 +1,64 @@
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const createTweet = (handle, text) => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text
+    })
+    .then(res => res.body);
+};
+
+describe('app tests', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  describe('tweets', () => {
+    it('creates a new tweet', () => {
+      return request(app)
+        .post('/tweets')
+        .send({ handle: 'ryan', text: 'my first tweet' })
+        .then(res => {
+          expect(res.body).toEqual({
+            handle: 'ryan',
+            text: 'my first tweet',
+            _id: expect.any(String)
+          });
+        });
+    });
+    it('gets a list of tweet by id', () => {
+      return createTweet('tweet1', 'I am a tweet')
+        .then(createdTweet => {
+          const id = createdTweet._id;
+          return request(app)
+            .get(`/tweets/${id}`);
+        })
+        .then(res => {
+          expect(res.body).toEqual({ handle:'tweet1', text: 'I am a tweet', _id: expect.any(String) });
+        });
+    });
+    it('gets a list of tweets from db', () => {
+      const tweetsToCreate = ['tweet1', 'tweet2', 'tweet3'];
+      return Promise.all(tweetsToCreate.map(createTweet))
+        .then(() => {
+          return request(app)
+            .get('/tweets');
+        })
+        .then(({ body }) => {
+          expect(body).toHaveLength(3);
+        });
+    });
+  });
+});
