cefef69afd598a093216c4b29c918941311449eb
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-18 16:15:44 -0800

    all tests working on tags

--- tests/app.test.js
+++ tests/app.test.js
@@ -13,13 +13,23 @@ const createTweet = (handle) => {
     .then(res => res.body);
 };
 
+const createTag = (tag) => {
+  return request(app)
+    .post('/tags')
+    .send({
+      tag: tag,
+    })
+    .then(res => res.body);
+};
+
+
+
 describe('tweets', () => {
   beforeEach(done => {
     rimraf('./data/tweets', err => {
       done(err);
     });
   });
-
   beforeEach(done => {
     mkdirp('./data/tweets', err => {
       done(err);
@@ -93,3 +103,79 @@ describe('tweets', () => {
   });
 });
 
+
+describe('tags', () => {
+  beforeEach(done => {
+    rimraf('./data/tags', err => {
+      done(err);
+    });
+  });
+  beforeEach(done => {
+    mkdirp('./data/tags', err => {
+      done(err);
+    });
+  });
+
+  it('gets a list of tags', () => {
+    const tagsToMake = ['#swag', '#BigBallerBrand', '#code'];
+    return Promise.all(tagsToMake.map(createTag))
+      .then(() => {
+        return request(app)
+          .get('/tags');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(3);
+      });
+  });
+  it('posts a tag', () => {
+    return request(app)
+      .post('/tags')
+      .send({ tag: '#yolo' })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          tag: '#yolo',  
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('gets a single tag by id', () => {
+    return createTag('#PokemonGo2ThePolls')
+      .then(createdTag => {
+        const _id = createdTag._id;
+        return request(app)
+          .get(`/tags/${_id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              tag: '#PokemonGo2ThePolls',
+              _id: _id
+            });
+          });
+      });
+  });
+  it('updates a tag by id', () => {
+    return createTag('#DabOnHaters')
+      .then(createdTag => {
+        const _id = createdTag._id;
+        return request(app)
+          .put(`/tags/${_id}`)
+          .send({ handle: '#JavaScript' })
+          .then(res => {
+            expect(res.body).toEqual({
+              handle: '#JavaScript',
+              _id: _id
+            });
+          });
+      });
+  });
+  it('deletes a tweet by id', () => {
+    return createTweet('#RoboRyan')
+      .then(createdTweet => {
+        const _id = createdTweet._id;
+        return request(app)
+          .delete(`/tags/${_id}`)
+          .then(res => {
+            expect(res.body).toEqual({ deleted: 1 });
+          });
+      });
+  });
+});
