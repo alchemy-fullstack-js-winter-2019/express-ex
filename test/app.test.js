4e24c670d4d3b0c3003deaac56fe3f7d65885d02
Ivan <i.poblete915@gmail.com>
2019-01-18 17:25:28 -0800

    able to get a list of all tweets

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,31 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 
-describe('tweets', () => {
+const createTweet = handle => {
+  return request(app)
+    .post('/tweets')
+    .send({
+      handle,
+      text: 'tweet this'
+    })
+    .then(res => res.body);
+};
+
+describe('app tests', () => {
+  beforeEach(done => {
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
+
   it('creates a new tweet', () => {
     return request(app)
       .post('/tweets')
@@ -16,6 +40,14 @@ describe('tweets', () => {
   });
 
   it('gets a list of tweets', () => {
-
+    const tweetsToCreate = ['yo!', 'Ayyyy', 'Hello', 'Banana!'];
+    return Promise.all(tweetsToCreate.map(createTweet))
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      }) 
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
+      });
   });
 });
