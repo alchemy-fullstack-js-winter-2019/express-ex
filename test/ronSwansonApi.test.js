ca2c64d089ce417bf7d8f12ec36b8d84d4e85cd2
Paige Gorry <paigeegorry@gmail.com>
2019-01-22 09:55:35 -0800

    res.reponseTime now correct in ms

--- test/ronSwansonApi.test.js
+++ test/ronSwansonApi.test.js
@@ -4,7 +4,6 @@ describe('ronSwansonApi', () => {
   it('can get random quotes', () => {
     return getRandomQuote(10)
       .then(res => {
-        console.log(res);
         expect(res).toHaveLength(10);
       });
   });
