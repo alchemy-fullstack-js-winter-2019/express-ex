8b2d8e0612e38d69b9a213b94ddd6ddba22c3d11
Masmics <cherlocker3113@gmail.com>
2019-01-22 15:36:52 -0800

    committing for PR

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const mkdirp = require('mkdirp');
-const rimraf = require('rimraf');
+// const mkdirp = require('mkdirp');
+// const rimraf = require('rimraf');
 
 const makeTweet = text => {
   return request(app)
@@ -15,15 +15,32 @@ const makeTweet = text => {
 };
 
 describe('tweets', () => {
-  beforeEach(done => {
-    rimraf('./data/tweets', err => {
-      done(err);
-    });
-  });
-  beforeEach(done => {
-    mkdirp('/data/tweets', err => {
-      done(err);
-    });
+//   beforeEach(done => {
+//     rimraf('./data/tweets', err => {
+//       done(err);
+//     });
+//   });
+//   beforeEach(done => {
+//     mkdirp('/data/tweets', err => {
+//       done(err);
+//     });
+//   });
+
+  it('gets a tweet by id', () => {
+    return makeTweet('Test Tweet')
+      .then(({ _id }) => {
+        return Promise.all([
+          Promise.resolve(_id),
+          request(app).get(`/tweets/${_id}`)
+        ]);
+      })
+      .then(([_id, { body }]) => {
+        expect(body).toEqual({
+          handle: 'ryan',
+          text: 'You got it by ID',
+          _id
+        });
+      });
   });
 
   it('creates and reads a tweet', () => {
@@ -40,17 +57,18 @@ describe('tweets', () => {
         });
       });
   });
-
-  it('gets all tweets', () => {
-    const testList = ['tweet1', 'tweet2', 'tweet3'];
-    return Promise.all(testList.map(makeTweet))
-      .then(() => {
-        return request(app)
-          .get('/tweets');
-      }) 
-      .then(({ body }) => {
-        expect(body).toHaveLength(3);
-      });
-  });
 });
+
+// it('gets all tweets', () => {
+//   const testList = ['tweet1', 'tweet2', 'tweet3'];
+//   return Promise.all(testList.map(makeTweet))
+//     .then(() => {
+//       return request(app)
+//         .get('/tweets');
+//     }) 
+//     .then(({ body }) => {
+//       expect(body).toHaveLength(3);
+//     });
+// });
+
 
