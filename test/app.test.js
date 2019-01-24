74a88360d26a7aebb13a84dd92f951faab9acab9
Carmen Perezchica <crperezchica@gmail.com>
2019-01-24 09:07:07 -0800

    fixing travis issues

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,4 @@
-const app = require('../../lib/app');
+const app = require('../lib/app');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 const request = require('supertest');
