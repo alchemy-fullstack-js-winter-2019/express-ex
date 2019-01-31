5a3905dd98c126ce016ec680920aad5597f418bb
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-30 19:33:21 -0800

    asdfasfd

--- test/tags.test.js
+++ test/tags.test.js
@@ -3,22 +3,28 @@ const app = require('../lib/app');
 const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 
+const createTag = (tag) => {
+  return request(app)
+    .post('/tags')
+    .send({
+      group: 'COOLTHINGS',
+      tag: tag
+    })
+    .then(res => {
+      return res.body;
+    });
+};
+
 describe('test tag routes', () => {
-  const createTag = (tag) => {
-    return request(app)
-      .post('/tags')
-      .send({ group: 'cool thing', tag })
-      .then(res => res.body);
-  };
   
   beforeEach(done => {
-    rimraf('./data/tweets', err => {
+    rimraf('./data/tags', err => {
       done(err);
     });
   });
   
   beforeEach(done => {
-    mkdirp('./data/tweets', err => {
+    mkdirp('./data/tags', err => {
       done(err);
     });
   });
@@ -29,6 +35,53 @@ describe('test tag routes', () => {
       .send({ group: 'hello', tag: 'super cool tag' })
       .then(res => {
         expect(res.body.group).toContain('hello');
+      });
+  });
+
+  it('can get a list of tags', () => {
+    const tags = ['tag1', 'tag2', 'tag3'];
+    return Promise.all(tags.map(createTag))
+      .then(() => {
+        return request(app)
+          .get('/tags');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(3);
+      });
+  });
+
+  it('can update a tag', () => {
+    return createTag('ALOHA')
+      .then(createdTag => {
+        return request(app)
+          .put(`/tags/${createdTag._id}`)
+          .send({ ...createdTag, tag: 'cool' });
+      })
+      .then(res => {
+        console.log(res.body);
+        expect(res.body.tag).toContain('cool');
+      });
+  });
+
+  it('can get a tag by id', () => {
+    return createTag('something')
+      .then(createdTag => {
+        return request(app)
+          .get(`/tags/${createdTag._id}`);
+      })
+      .then(res => {
+        expect(res.text).toContain('something');
+      });
+  });
+
+  it('can delete a tag', () => {
+    return createTag('fooBar')
+      .then(createdTag => {
+        return request(app)
+          .del(`/tags/${createdTag._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 'deleted': 1 });
       });
   });
 });
